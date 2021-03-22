d68df116c88e3916d9cf60b63f824e873caf9aa3
jessicalh <jessicalynnehansen@gmail.com>
2021-03-21 20:02:53 -0600

    challenge complete
    .

--- src/App.js
+++ src/App.js
@@ -4,10 +4,22 @@ import "./styles.css";
 export default function App() {
   const [spacing, setSpacing] = useState(10);
   const [blur, setBlur] = useState(10);
-  const [base, setBase] = useState('yellow');
-  
+  const [base, setBase] = useState("#bada55");
+
+  const onChangeSpacing = (event) => {
+    setSpacing(event.target.value);
+  };
+
+  const onChangeBlur = (event) => {
+    setBlur(event.target.value);
+  };
+
+  const onChangeBase = (event) => {
+    setBase(event.target.value);
+  }
+
   return (
-    <div>
+    <div style={{ "--spacing": `${spacing}px`, "--blur": `${blur}px`, "--base": `${base}` }}>
       <h2>
         Update CSS Variables with <span className="hl">JS</span>
       </h2>
@@ -20,6 +32,7 @@ export default function App() {
           min="10"
           max="200"
           value={spacing}
+          onChange={onChangeSpacing}
         />
 
         <label htmlFor="blur">Blur:</label>
@@ -30,15 +43,17 @@ export default function App() {
           min="0"
           max="25"
           value={blur}
+          onChange={onChangeBlur}
         />
 
         <label htmlFor="base">Base Color</label>
-        <input id="base" type="color" name="base" value="#ffc600" />
+        <input id="base" type="color" name="base" value={base} onChange={onChangeBase} />
       </div>
 
       <img
         src="https://source.unsplash.com/7bwQXzbF6KE/800x500"
         alt="person on mountain"
+        style={{ padding: `${spacing}px` }}
       />
     </div>
   );
